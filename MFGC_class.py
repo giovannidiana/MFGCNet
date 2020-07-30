cddb8999e32591e6b476a6429f3b6552bcbaa747
rosiejarrett <68652429+rosiejarrett@users.noreply.github.com>
2020-07-31 00:18:33 +0100

    Update MFGC_class.py
    I am confused as I know you ran the code on your IDE but I cannot get the plt.show()(line 57) to work for the neural network but there are no error messages. I suspect it is some kind of looping issue as when I indent my test plot(59-63) to the same indentation as your plt.show() it stops bringing my testing graph up. However, changing the indentation of line 57 does not solve the issue.

--- MFGC_class.py
+++ MFGC_class.py
@@ -3,7 +3,9 @@
 import numpy as np
 from numpy import random
 import networkx as nx
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
 
 random.seed( 1 )
 
@@ -16,6 +18,15 @@ class MFGC:
         self.connectionArray = np.zeros(shape=[nGC,4],dtype=np.int)
 
         ## assign MF labels, please
+        self.MFTYPES = np.array([1,2])
+
+        labelMFDict = {}
+        for loopMF in np.arange(nMF):
+            labelMFDict.update({loopMF: np.random.choice(self.MFTYPES)})
+            #I made a dictionary to randomly assign types to each MF 
+
+        print (labelMFDict)
+        print (labelMFDict.get(2))
 
         self.wire()
 
@@ -32,18 +43,24 @@ class MFGC:
         center_MFL = (self.nMF)/2 - 1
         center_GCL = (self.nGC)/2 - 1
         for i in np.arange(self.nMF):
-            G.add_node(MFnodeNames[i],pos=(0,(i-center_MFL)))
+            G.add_node(MFnodeNames[i],pos=(0,(i-center_MFL)), color="red")
 
         for i in np.arange(self.nGC):
-            G.add_node(GCnodeNames[i],pos=(.5,(i-center_GCL)))
+            G.add_node(GCnodeNames[i],pos=(.25,(i-center_GCL)))
 
         for gc in np.arange(self.nGC):
             for i in np.arange(4):
                 G.add_edge(GCnodeNames[gc],MFnodeNames[self.connectionArray[gc,i]])
 
 
-        nx.draw(G,nx.get_node_attributes(G,'pos'),node_size=10)
+        nx.draw(G,nx.get_node_attributes(G,'pos'),node_size=10, alpha=0.4)
         plt.show()
+        
+    #I used this to test the plotting functions but I have tried as many solutions as I could find and i cannot get your plt.show() to show the graph of the connections and nodes
+    plt.plot([1,2,3,4],[1,4,6,8], alpha=0.5)
+    plt.plot([2,4,6,8],[3,6,9,12], color="green", alpha = 0.5)
+    plt.ylabel('some numbers', color="red")
+    plt.show()
 
 
 nGC = 50
